b846129802cdf3745a31bfe3a4b8842e8b33f774
sangrampawar9 <svp1562@rit.edu>
2017-06-19 19:38:01 -0400

    new portfolio

---
+++ portfoliosp/itemtrial/main.js
@@ -0,0 +1,32 @@
+'use strict'; 
+$(window).load( function() {	
+    
+
+   $(document).ready( function() {	
+    
+    
+    
+    
+    
+    // HOME TYPED JS
+     $(function(){
+      $(".element").typed({
+        strings: ["UX Designer", "Front-end Developer"],
+        typeSpeed: 10,
+        loop:true,
+          
+        backDelay: 1500
+      });
+  });
+     $(".element").css("color","#ffd504");
+    
+    
+    
+}); // document ready end 
+
+    
+
+    
+}); // window load end 
+
+
